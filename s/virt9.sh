0b53b20c01fffb1464e6fd4da3113fba1a2d6790
Edoardo La Greca <elagreca@protonmail.com>
2023-09-13 18:12:03 +0200

    make iso argument optional

--- s/virt9.sh
+++ s/virt9.sh
@@ -1,14 +1,18 @@
 #! /bin/sh
 
-iso=$1
-disk=$2
+disk=$1
+iso=$2
+
+if [ -n "$iso" ]
+then
+	isoargs="-cdrom $iso -boot cd"
+fi
 
 qemu-system-x86_64 \
 	-m 1024M \
 	-hda $disk \
-	-cdrom $iso \
+	$isoargs \
 	-k en-us \
-	-boot dc \
 	-usb \
 	-netdev user,id=net0 -device e1000,netdev=net0,mac=52:54:98:76:54:32 \
 	-nic user,model=e1000,mac=52:54:98:76:54:32 \
