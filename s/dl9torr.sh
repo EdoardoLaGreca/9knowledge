97c242d7573205e215a4655fca55504049bc6d47
Edoardo La Greca <edo@edolg.net>
2024-11-22 16:51:54 +0100

    add dl9torr.sh

---
+++ s/dl9torr.sh
@@ -0,0 +1,35 @@
+#! /bin/sh
+
+# Download the latest 9front release torrent file using Curl.
+
+dom='9front.org'
+dldom='iso.only9fans.com'
+
+which curl >/dev/null
+if [ $? -eq 127 ]
+then
+	echo 'cannot run curl :(' >&2
+	exit 1
+fi
+
+# like grep -o but posix-compliant
+xtract() {
+	patt="$1"
+	while read line
+	do
+		echo "$line" | grep -q "$patt" || continue
+		befaft=$(echo "$line" | sed "s|$patt|\n|")
+		bef=$(echo "$befaft" | head -n 1)
+		aft=$(echo "$befaft" | tail -n 1)
+		echo "$line" | sed "s|^$bef||;s|$aft\$||"
+	done
+}
+
+# latest donwload page link
+plink=$(curl "https://$dom" | xtract "http://$dom/releases/[[:digit:]/]*" | sed 's/^http:/https:/')
+
+# direct download link
+dlink=$(curl "$plink" | xtract "http://$dldom/release/9front-[[:digit:]]*\.amd64\.iso\.gz\.torrent" | sed 's/^http:/https:/')
+
+curl -O "$dlink"
+
