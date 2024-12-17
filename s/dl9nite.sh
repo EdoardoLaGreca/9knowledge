1ce502aa42d9558df4c9ae7862f48ada40a9b673
Edoardo La Greca <edo@edolg.net>
2024-12-17 16:47:14 +0100

    refactor and fix url

--- s/dl9nite.sh
+++ s/dl9nite.sh
@@ -6,12 +6,12 @@
 
 ckcurl
 
-link=$(match "https://$dldom" "/9front/9front-[[:digit:]]*.amd64.iso.gz")
-if [ -z "$link" ]
+path=$(match "https://$dldom" "/9front/9front-[[:digit:]]*.amd64.iso.gz")
+if [ -z "$path" ]
 then
 	echo 'cannot find download url' >&2
 	exit 1
 fi
 
-curl -O "https://$dldom/$link"
+curl -O "https://$dldom""$path"
 
