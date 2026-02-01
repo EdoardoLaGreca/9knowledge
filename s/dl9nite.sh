eac9c357435a5109a1c4ec0748ec25648d7ee119
Edoardo La Greca <edo@edolg.net>
2026-02-01 20:11:50 +0100

    refactoring

--- s/dl9nite.sh
+++ s/dl9nite.sh
@@ -17,12 +17,12 @@ ckcurl
 
 echo "searching for the file's url... (might take a while)"
 
-path=$(match "https://$dldom" "/9front/9front-[[:digit:]]*.amd64.iso.gz")
-if [ -z "$path" ]
+url=$(match "$purl" "${dpath}/9front-[[:digit:]]*\.amd64\.iso\.gz" | sed "s|^|https://$nitedom|")
+if [ -z "$url" ]
 then
 	echo 'cannot find download url' >&2
 	exit 1
 fi
 
-curldl "https://$dldom""$path"
+curldl "$url"
 
--- s/dl9torr.sh
+++ s/dl9torr.sh
@@ -14,21 +14,21 @@ ckcurl
 
 echo "searching for the file's url... (might take a while)"
 
-# latest download page link
-plink=$(match "https://$dom" "http://$dom/releases/[[:digit:]/]*" | sed 's/^http:/https:/')
-if [ -z "$plink" ]
+# latest download page url
+purl=$(match "https://$dom" "http://${dom}${ppath}/[[:digit:]/]*" | sed 's/^http:/https:/')
+if [ -z "$purl" ]
 then
 	echo 'cannot find the download page' >&2
 	exit 1
 fi
 
-# direct download link
-dlink=$(match "$plink" "http://$dldom/release/9front-[[:digit:]]*\.amd64\.iso\.gz\.torrent" | sed 's/^http:/https:/')
-if [ -z "$dlink" ]
+# direct download url
+durl=$(match "$purl" "http://${dom}${dpath}/9front-[[:digit:]]*\.amd64\.iso\.gz\.torrent" | sed 's/^http:/https:/')
+if [ -z "$durl" ]
 then
 	echo 'cannot find the download url' >&2
 	exit 1
 fi
 
-curldl "$dlink"
+curldl "$durl"
 
