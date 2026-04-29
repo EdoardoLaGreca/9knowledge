f88aaec4fb4b7df8ee1ae7e9fbf5ada3d7027873
Edoardo La Greca <edo@edolg.net>
2026-04-29 17:31:26 +0200

    improve documentation

--- s/virt/README.md
+++ s/virt/README.md
@@ -1,7 +1,9 @@
 Virtualisation
 ==============
 
-To create a fully virtualised 9front environment, use virt9.sh. Look at its description for more info, first comment in the file.
+To create a fully virtualised 9front environment, use **virt9.sh**. Look at its description for more info, first comment in the file.
+
+This script intentionally leaves out non-essential features, such as audio, and only focuses on the emulation of CPU, basic graphics, and networking. The idea is that you should have some way to boot 9front and start the rcpu service, but then you should use drawterm for everything else.
 
 Currently supported platforms:
 
--- s/virt/virt9.sh
+++ s/virt/virt9.sh
@@ -1,24 +1,21 @@
 #! /bin/sh
 
-# Start a 9front virtual machine using QEMU on GNU/Linux. Specify the path to
-# the main disk image as first argument. An ISO image may be specified as second
-# argument.
+# Start a 9front virtual machine. Specify the path to the main QCOW2 disk image
+# as first argument. An ISO image may be specified as second argument.
 # 	./virt9.sh disk-image.qcow2 [ cd-image.iso ]
-# To create a disk image, use:
-# 	qemu-img create -f qcow2 <name>.qcow2 <size>G
-# This script intentionally leaves out non-essential features, such as audio,
-# and only focuses on the emulation of CPU, basic graphics, and networking. The
-# idea is that you should have some way to boot 9front and start the rcpu
-# service, but then you should use drawterm for everything else.
-# This script also provides some degree of customisation through environment
+# This script provides some degree of customisation through environment
 # variables. It is suggested to pass them to the script using the prefix syntax,
 # i.e.:
 #	name1=value1 [ name2=value2 ... ] ./virt9.sh ...
-# Variables typically have the same name as the option they are applied to. For
-# example, the 'display' variable is used for QEMU's '-display' option. Once
-# everything is set up and just before launching QEMU, all customisable
-# variables are printed to standard output with their respective values in a
-# familiar syntax.
+# The variables are:
+# - `disk`: main disk storage in QCOW2 format)
+# - `iso`: optional installation ISO image
+# - `display`: the kind of window created for graphics (QEMU only)
+# - `smp`: the amount of CPUs assigned in multi-core environments
+# - `mem`: the amount of volatile memory allocated
+# Once everything is set up and just before launching the hypervisor, all
+# customisable variables are printed to standard output with their respective
+# values in a familiar syntax.
 
 # You may want to assign these two variables below to known paths in your file
 # system, to save some time and effort.
